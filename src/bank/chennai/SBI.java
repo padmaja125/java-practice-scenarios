e23708b1ea91b12366033fab568b1a665b21dbc1
Padmaja <padhu.ramsh@gmail.com>
2024-06-24 16:45:44 -0400

    default with static & non-static access modifier

---
+++ src/bank/chennai/SBI.java
@@ -0,0 +1,21 @@
+package bank.chennai;
+
+public class SBI {
+    String empName, empId;
+    static String branch_name ="chennai";
+    void get_loan(int amount){
+    }
+    void create_account(){
+
+    }
+
+}
+/** Access Modifiers:
+ Private: Accessible only within the class it is defined.
+ Default (Package-Private): Accessible only within the same package.
+ Protected: Accessible within the same package and subclasses.
+ Public: Accessible from any other class.
+ Static Variables: Shared among all instances of a class.
+ Static Methods: Belong to the class rather than any object instance.
+ Non-Static Methods and Variables: Belong to an object instance.
+ Packages: Organize classes into namespaces providing controlled access to class members. **/
---
+++ src/bank/madurai/Account_Holder_madurai.java
@@ -0,0 +1,19 @@
+package bank.madurai;
+
+import bank.chennai.SBI;
+
+public class Account_Holder_madurai {
+    public static void main(String[] args) {
+        SBI account_holder = new SBI();
+        // compiler error 'create_account()',empId,empName,branch_name,'get_loan' is not public in 'bank.chennai.SBI'. Cannot be accessed from outside package
+/*        account_holder.empId = "1324";
+        account_holder.empName = "Smith";
+
+        System.out.println(account_holder.empId);
+        System.out.println(account_holder.empName);
+        //account_holder.create_account();
+        //account_holder.get_loan(200);
+
+        System.out.println(account_holder.branch_name);*/
+    }
+}
